8f93baad690aaeef9b2dbd475ed167316200265c
hybridreaver1065 <42978468+hybridreaver1065@users.noreply.github.com>
2018-09-17 14:28:20 -0400

    Add files via upload

---
+++ CalculatorMain.java
@@ -0,0 +1,12 @@
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		
+		Calculustor calc = new Calculustor();
+		
+		
+		
+		System.out.print(calc.add(17, 38))
+	}
+}
---
+++ Calculustor.java
@@ -0,0 +1,21 @@
+
+public class Calculustor {
+	
+	
+	
+	
+	public int add(int num1, int num2) {
+		int ans = num1 + num2;
+		return ans;
+	}
+		
+		
+		
+		
+		
+		
+			
+	
+	
+	
+}
